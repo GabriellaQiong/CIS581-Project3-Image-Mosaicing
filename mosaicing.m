eca01c6dd7372c1bf25a6c6e3422839193fde209
qiong <qiong@qiong-Yoga-13.(none)>
2013-11-09 18:53:47 -0500

    add fig_save.m and corresponding plotting codes

--- mosaicing.m
+++ mosaicing.m
@@ -1,8 +1,9 @@
-function mosaicedIm = mosaicing(im1, im2)
+function mosaicedIm = mosaicing(im1, im2, iter)
 % MOSIACING stitches two images into one
 
 % INPUT
-% im1, im2     = input rgb images
+% im1, im2   = input rgb images
+% iter       = iteration number for multiple image stitching
 
 % OUTPUT
 % mosaicedIm = stitched color image between original two color images
@@ -10,10 +11,19 @@ function mosaicedIm = mosaicing(im1, im2)
 % Written by Qiong Wang at University of Pennsylvania
 % Nov. 9th, 2013
 
+% Path
+p = mfilename('fullpath');
+funcDir = fileparts(p);
+outputDir = fullfile(funcDir, '/results');
+if ~exist(outputDir, 'dir')
+    mkdir(outputDir); 
+end 
+
 % Parameters
 max_pts       = 200;
 geometricBlur = false;
 threshRANSAC  = 6;
+verbose       = true;
 
 % Parsing images
 I1 = rgb2gray(im1);
@@ -32,9 +42,20 @@ p1 = feat_desc(I1, y1, x1, geometricBlur);
 p2 = feat_desc(I2, y2, x2, geometricBlur);
 
 % Feature matching
-m = feat_match(p1,p2);
+m  = feat_match(p1,p2);
+Y1 = y1(m ~= -1);
+X1 = x1(m ~= -1);
+Y2 = y2(m(m ~= -1));
+X2 = x2(m(m ~= -1));
 
 % Estimate homography by RANSAC
-[H, inlier_ind] = ransac_est_homography(y1, x1, y2, x2, threshRANSAC);
+[H, inlier_ind] = ransac_est_homography(Y1, X1, Y2, X2, threshRANSAC);
 
-end 
+
+% Save results
+fileString = fullfile(outputDir, ['stitched', num2str(iter,'%02d')]);
+h = figure();
+
+fig_save(h, 'png', fileString);
+
+end
--- ransac_est_homography.m
+++ ransac_est_homography.m
@@ -1,18 +1,22 @@
-function [H, inlier_ind] = ransac_est_homography(y1, x1, y2, x2, thresh)
+function [H, inlier_ind] = ransac_est_homography(y1, x1, y2, x2, thresh, iter, verbose)
 % RANSAC_EST_HOMOGRAPHY estimates the homography between two corresponding
 % feature points through RANSAC. im2 is the source and im1 is the destination.
 
 % INPUT
 % y1,x1,y2,x2 = corresponding point coordinate vectors Nx1
 % thresh      = threshold on distance to see if transformed points agree
+% iter        = iteration number for multiple image stitching
 
 % OUTPUT
 % H           = the 3x3 matrix computed in final step of RANSAC
 % inlier_ind  = nx1 vector with indices of points that were inliers
 
-
 % Written by Qiong Wang at University of Pennsylvania
 % Nov. 9th, 2013
+
+if nargin < 3
+   verbose = false; 
+end
 
 % Initialize
 N        = size(y1, 1);
@@ -37,5 +41,41 @@ end
 % Find the best score
 [~, best]  = max(score);
 H          = H{best};
-inlier_ind = find(ok{best})';
+ok         = ok{best};
+inlier_ind = find(ok)';
+
+% Plot the verbose details
+if ~verbose
+    return
+end
+
+dh1 = max(size(im2,1)-size(im1,1),0);
+dh2 = max(size(im1,1)-size(im2,1),0);
+
+h = figure(1); 
+clf;
+
+% Original Matches
+subplot(2,1,1);
+imagesc([padarray(im1,dh1,'post') padarray(im2,dh2,'post')]);
+delta = size(im1,2);
+line([x1; x2 + delta], [y1; y2]);
+title(sprintf('%d Original matches', N));
+axis image off;
+
+% Inlier Matches
+subplot(2,1,2);
+imagesc([padarray(im1,dh1,'post') padarray(im2,dh2,'post')]);
+delta = size(im1,2);
+line([x1(inlier_ind); x2(inlier_ind) + delta], [y1(inlier_ind); y2(inlier_ind)]);
+title(sprintf('%d (%.2f%%) inliner matches out of %d', sum(ok), 100*sum(ok)/N, N));
+axis image off;
+drawnow;
+
+% Save the figures
+p = mfilename('fullpath');
+funcDir = fileparts(p);
+outputDir = fullfile(funcDir, '/results');
+fileString = fullfile(outputDir, ['matches', num2str(iter,'%02d')]);
+fig_save(h, fileString, 'png');
 end
